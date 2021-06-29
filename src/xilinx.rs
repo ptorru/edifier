98321d0359d9bd3b2e9800c1d40af4bf17545023
Pedro Torruella <ptorru14@gmail.com>
2021-06-29 14:07:32 +0000

    First functions to support location in xilinx

--- src/xilinx.rs
+++ src/xilinx.rs
@@ -1,9 +1,6 @@
 use crate::ast::*;
 
-
-pub fn new_lut2() -> Cell
-{
-
+pub fn new_lut2() -> Cell {
     let diro = PortElements::Direction(PortDirection::Output);
     let porto = InterfacePort {
         name: "O".to_string(),
@@ -31,6 +28,40 @@ pub fn new_lut2() -> Cell
     };
     Cell {
         name: "LUT2".to_string(),
-        views: CellViews(vec![cellview])
+        views: CellViews(vec![cellview]),
+    }
+}
+
+/*
+(property INIT (string "4'h6"))
+(property BOX_TYPE (string "PRIMITIVE"))
+(property LOC (string "SLICE_X0Y0"))
+(property BEL (string "A6LUT"))*/
+
+pub fn lut2_prop_ini(val: String) -> Property{
+    Property {
+        name: "INIT".to_string(),
+        property: PropertyValue::String(val),
+    }
+}
+
+pub fn lut2_prop_box(val: String) -> Property{
+    Property {
+        name: "BOX_TYPE".to_string(),
+        property: PropertyValue::String(val),
+    }
+}
+
+pub fn lut2_prop_loc(val: String) -> Property{
+    Property {
+        name: "LOC".to_string(),
+        property: PropertyValue::String(val),
+    }
+}
+
+pub fn lut2_prop_bel(val: String) -> Property{
+    Property {
+        name: "BEL".to_string(),
+        property: PropertyValue::String(val),
     }
 }
--- tests/xilinx_ut.rs
+++ tests/xilinx_ut.rs
@@ -1,15 +1,43 @@
+use edifier::ast::*;
 use edifier::xilinx::*;
 
-// Test 1: we should get a minimal edif with no elements
+// Test 1: we should get a lut2 element
 #[test]
 fn lut2() {
     let ed = new_lut2();
-    
+
     let actual = serde_sexpr::to_string(&ed).unwrap();
-    
+
     assert_eq!(
         actual,
         "(cell LUT2 (celltype GENERIC) (view netlist (viewtype NETLIST) (interface (port O (direction OUTPUT)) (port I0 (direction INPUT)) (port I1 (direction INPUT)))))"
     );
     //assert_eq!(match_check(actual), 0);
+}
+
+// Test 2: we should get an instance with properties 
+//         for a placed lut2 element
+#[test]
+fn lut2_instance() {
+
+    let mut proplist = PropertyList(Vec::new());
+    proplist.push(lut2_prop_ini("4'h6".to_string()));
+    proplist.push(lut2_prop_box("PRIMITIVE".to_string()));
+    proplist.push(lut2_prop_loc("SLICE_X0Y0".to_string()));
+    proplist.push(lut2_prop_bel("A6LUT".to_string()));
+
+    let contents = ContentInstance {
+        name: "i0".to_string(),
+        viewref: "netlist".to_string(),
+        cellref: "LUT2".to_string(),
+        libraryref: "hdi_primitives".to_string(),
+        properties: proplist,
+    };
+
+    let actual = serde_sexpr::to_string(&contents).unwrap();
+
+    assert_eq!(
+        actual,
+        r#"(instance i0 (viewref netlist (cellref LUT2 (libraryref hdi_primitives))) (property INIT (string "4'h6")) (property BOX_TYPE (string "PRIMITIVE")) (property LOC (string "SLICE_X0Y0")) (property BEL (string "A6LUT")))"#
+    );
 }
