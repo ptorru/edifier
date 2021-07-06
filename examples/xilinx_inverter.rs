abedff9ff31887d80e7f41c6d0e6824b345f215f
Pedro Torruella <ptorru14@gmail.com>
2021-07-06 16:39:02 +0000

    Split into function to reuse

--- examples/xilinx_inverter.rs
+++ examples/xilinx_inverter.rs
@@ -1,5 +1,11 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::Write;
 use edifier::ast::*;
 use edifier::string_helpers::add_new_lines;
+
+
+static DOFILE: bool = true;
 
 fn main() {
     let top_name = "inverter".to_string();
@@ -106,7 +112,26 @@ fn main() {
         elements: EdifElements::from(vec![libp, libw, desi]),
     };
 
-    // let edif_string = serde_sexpr::to_string(&edif).unwrap();
-    let edif_string = add_new_lines(serde_sexpr::to_string(&edif).unwrap(), 5, true);
-    println!("{}", edif_string);
+    let serialized = serde_sexpr::to_string(&edif).unwrap();
+
+    if DOFILE {
+        let path = Path::new("xilinx_inverter.edf");
+        let display = path.display();
+
+        // Open a file in write-only mode, returns `io::Result<File>`
+        let mut file = match File::create(&path) {
+            Err(why) => panic!("couldn't create {}: {}", display, why),
+            Ok(file) => file,
+        };
+
+        match file.write_all(serialized.as_bytes()) {
+            Err(why) => panic!("couldn't write to {}: {}", display, why),
+            Ok(_) => println!("successfully wrote to {}", display),
+        }
+    } else {
+        // let edif_string = serde_sexpr::to_string(&edif).unwrap();
+        let edif_string = add_new_lines(serialized, 5, true);
+        println!("{}", edif_string);
+    };
+
 }
